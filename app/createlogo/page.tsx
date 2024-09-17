12f9932ff7508a566b6c81857b9cb5703b0a7e9a
rupesh <rpiwal02@gmail.com>
2024-09-17 14:53:09 +0530

    created routes and components

---
+++ app/createlogo/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default page;
